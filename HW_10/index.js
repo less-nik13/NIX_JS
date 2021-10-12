0701c2aeeb3b33e5e1b634cd4ed1b7e7c8ea54ac
less-nik13 <62059273+less-nik13@users.noreply.github.com>
2021-10-12 12:38:39 +0300

    refactor: change getData fn, and createCard fn

--- HW_10/index.js
+++ HW_10/index.js
@@ -1,72 +1,77 @@
 const cards = document.getElementById('cards');
 
-const url = 'https://api.punkapi.com/v2/beers?brewed_before=11-2017&abv_gt=6';
+const API_URL = 'https://api.punkapi.com/v2/beers?brewed_before=11-2017&abv_gt=6';
 
-async function getData(url) {
+async function getData() {
     localStorage.clear();
     try {
-        const response = await fetch(url);
-        if (response.ok) {
-            const data = await response.json();
-            createCard(data);
-        } else {
-            throw 'Server is not responding';
-        }
+        const response = await fetch(API_URL);
+        const data = await response.json();
+        createCard(data);
     } catch (err) {
+        cards.innerHTML = `<h2 style="margin-top: 100px">Nothing not found</h2>`
         console.log(err);
     }
 }
 
-getData(url);
+getData();
 
 function createCard(data) {
     data.forEach((el) => {
         let cardEl = document.createElement('li');
         let cardInnerHTML;
 
-        cardEl.classList = 'card';
+        cardEl.classList.add('card');
 
-        cardInnerHTML = `<div class="card-image">
-        <img data-image_id=${el.id} src="${el.image_url}"
-            alt="rover" />
+        cardInnerHTML = `
+        <div class="card-image">
+            <img data-image_id=${el.id} src="${el.image_url}" alt="rover" />
         </div>
         <div class="card-body">
             <span class="tag tag-teal">${el.contributed_by}</span>
-            <h4>
-                ${el.name}
-            </h4>
-            <p class="description">
-                ${el.description}
-            </p>
+            <h4>${el.name}</h4>
+            <p class="description">${el.description}</p>
             <h4>Food Pairs:</h4>
-            <ul id="food">`;
+            <ul id="food">
+        `;
 
         const foodPairing = el.food_pairing;
         foodPairing.forEach((food) => {
-            cardInnerHTML += `<li class="food-item">
-            ${food}
-            </li>
-            `;
+            cardInnerHTML += `<li class="food-item">${food}</li>`;
         });
 
         cardInnerHTML += `
-        </ul>
+                </ul>
                 <div class="details">
-                <p>Alcohol by Volume: <span class="small">${el.abv}</span></p>
-                <p>First brewed: <span class="small">${el.first_brewed}</span></p>
+                    <p>Alcohol by Volume: <span class="small">${el.abv}</span></p>
+                    <p>First brewed: <span class="small">${el.first_brewed}</span></p>
                 </div>
-            </div>`;
+            </div>
+        `;
 
         cardEl.innerHTML = cardInnerHTML;
-
         cards.append(cardEl);
-
-        document
-            .querySelectorAll('.card-image > img')
-            .forEach((img) => img.addEventListener('click', addToLocalStorage));
     });
+    document
+        .querySelectorAll('.card-image > img')
+        .forEach((img) => img.addEventListener('click', addToLocalStorage));
 }
 
-function addToLocalStorage(el) {
-    localStorage.setItem(`imageId${el.target.dataset.image_id}`, el.target.dataset.image_id);
+function addToLocalStorage(e) {
+    const identifiers = [];
+    if (!localStorage.getItem('identifiers')) {
+        identifiers.push({imageId: e.target.dataset.image_id});
+        localStorage.setItem('identifiers', JSON.stringify(identifiers));
+    } else {
+        const localStorageArray = JSON.parse(localStorage.getItem('identifiers'));
+        if (localStorageArray.some(el => el.imageId === e.target.dataset.image_id)) {
+            localStorage.setItem('identifiers', JSON.stringify(localStorageArray));
+        } else {
+            identifiers.push(...JSON.parse(localStorage.getItem('identifiers')), {imageId: e.target.dataset.image_id});
+            localStorage.setItem(
+                'identifiers',
+                JSON.stringify(identifiers),
+            );
+        }
+    }
 }
